dfa88aade34d5d525a9037deff036bff49972707
xjgomez <jordi.aneas@bsc.es>
2017-12-04 12:44:38 +0100

    Pre scripts renaming.

--- misc/scripts/launch/ear_conf_learning_phase.sh
+++
@@ -1,9 +0,0 @@
-source $EAR_INSTALL_PATH/conf/ear_conf.sh
-
-export EAR_DB_PATHNAME=$EAR_INSTALL_PATH/DB_Learning_phase/EAR_App_learning_phase
-export EAR_LEARNING_PHASE=1
-export EAR_POWER_POLICY=MONITORING_ONLY
-
-
-
-
