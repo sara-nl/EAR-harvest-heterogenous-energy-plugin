f06449b96dcfaa44627f4b726b31b8fa31c28787
xjgomez <jordi.aneas@bsc.es>
2017-12-05 10:47:00 +0100

    Launching EAR applications without SLURM with etc/examples/ear_run.sh script.

--- etc/examples/ear_run.sh
+++ etc/examples/ear_run.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 
+function refresh_vars()
+{
+source environment/ear_env_libs.sh
+source environment/ear_env_conf.sh
+export HOSTLIST="$(echo $(cat hostlist))"
+}
+
+function daemon_ssh_start()
+{
+refresh_vars
+
+for i in ${HOSTLIST}
+do
+        echo "Executing ear_daemon in node=${i} p_state=$2 tmpdir=${EAR_TMP} verbose=${EAR_VERBOSE}"
+        ssh ${i} sudo LD_LIBRARY_PATH="${CPUPOWER_LIB_PATH}:${FREEIPMI_LIB_PATH}:${PAPI_LIB_PATH}:${LD_LIBRARY_PATH}" \
+        EAR_DB_PATHNAME=${EAR_DB_PATHNAME} $EAR_INSTALL_PATH/sbin/eard $1 ${EAR_TMP} ${EAR_VERBOSE} &
+done
+}
+
+function daemon_ssh_exit()
+{
+refresh_vars
+
+for i in ${HOSTLIST}
+do
+        DAEMON_PID=`sudo ssh $i ps -eo pid,comm | grep eard | awk '{print $1}'`
+        echo "Closing ear_daemon in ${i} with pid $DAEMON_PID"
+        ssh $i sudo LD_LIBRARY_PATH="" kill -TERM $DAEMON_PID &
+done
+}
+
 function mpirun_with_ear() { ### function to execute Intel_MPI code with EAR
 if [[ $# -eq 0 ]]
 then
-  echo -e "\n\tfunction $FUNCNAME to execute Intel_MPI code with EAR (energy policy=min_energy_to_solution) \n"
-  echo -e "\t\tUsage: $FUNCNAME binary nmpi ppn "
-  echo -e "\t\t\tbinary : name of local binary to run "
-  echo -e "\t\t\tmpi : number of total MPI tasks to use "
-  echo -e "\t\t\tppn : number of MPI tasks per node "
+  echo -e "\n\tfunction $FUNCNAME to execute Intel_MPI code with EAR\n"
+  echo -e "\t\tUsage: $FUNCNAME binary nmpi ppn policy"
+  echo -e "\t\t\tbinary: name of local binary to run"
+  echo -e "\t\t\tmpi: number of total MPI tasks to use"
+  echo -e "\t\t\tppn: number of MPI tasks per node"
+  echo -e "\t\t\tpolicy: 0 (min_energy) | 1 (min_time) | 2 (monitoring_only)"
   echo -e "\n\n"
 return 0
 fi
 
-# Edit region
+refresh_vars
+
+#
 export EAR_POWER_POLICY=MONITORING_ONLY
-export EAR_LIBRARY_GUI_PATH=$EAR_INSTALL_PATH/lib/libear.so
+if [ $4 -eq 0 ]
+then
+	export EAR_POWER_POLICY=MIN_ENERGY_TO_SOLUTION
+fi
+if [ $4 -eq 1 ]
+then
+	export EAR_POWER_POLICY=MIN_TIME_TO_SOLUTION
+fi
 
 # Non-edit region
-source ear_env_libs.sh
-source ear_env_conf.sh
-HOSTLIST=$(echo $(cat ./hostlist))
-
 local binary=${1:-"1"}
 local mpi=${2:-"1"}
 local ppn=${3:-"1"}
@@ -29,16 +66,14 @@ local def_freq=1
 ### BSC-Lenovo  Energy aware library:
 local a=$(date +%s) ; date
 
-
 ## Sarting EAR daemon: 1 ear_daemon per node
-source ear_daemon_start.sh hostlist ${def_freq}
-sleep 4
+daemon_ssh_start ${def_freq}
 
 ## Starting the application
-mpiexec.hydra -l -genv LD_PRELOAD=$EAR_LIBRARY_GUI_PATH -genvall -f ./hostlist -n ${mpi} -ppn=${ppn} ${binary}
+mpiexec.hydra -l -genv LD_PRELOAD=${EAR_LIB_PATH} -genvall -f ./hostlist -n ${mpi} -ppn=${ppn} ${binary}
 
 ## Just in case the app fails, we finish EAR demon
-##source ear_daemon_exit.sh hostlist
+daemon_ssh_exit
 
 local b=$(date +%s) ; date
 echo "Total elapsed time = $(( b - a )) sec "
--- etc/examples/environment/ear_env_libs.sh
+++ etc/examples/environment/ear_env_libs.sh
@@ -3,3 +3,5 @@
 export CPUPOWER_LIB_PATH="/usr/lib64"
 export FREEIPMI_LIB_PATH="/home/xjcorbalan/lib"
 export PAPI_LIB_PATH="/hpc/base/ctt/packages/papi/5.4.3/lib"
+export EAR_LIB_PATH="${EAR_INSTALL_PATH}/lib/libear.so"
+export EAR_TRAC_LIB_PATH="${EAR_INSTALL_PATH}/lib/libeartrac.so"
