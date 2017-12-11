9f3283b6cfad34bbfe68727c53737e3cd61645a0
xjgomez <jordi.aneas@bsc.es>
2017-12-11 12:50:15 +0100

    Examples installation added.

--- etc/examples/example_dgemm.sh
+++ etc/examples/example_dgemm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo nxt0347 > hostlist
-source ear_run.sh
+./ear_daemon_start.sh 1
+sleep 2
 export MKL_NUM_THREADS=28
-mpirun_with_ear /home/xjaneas/_ear/bin/kernels/dgemm_example 1 1 MONITORING_ONLY
+./ear_run.sh /home/xjaneas/_ear/bin/kernels/dgemm_example 1 1 MONITORING_ONLY
+sleep 2
+./ear_daemon_exit.sh
