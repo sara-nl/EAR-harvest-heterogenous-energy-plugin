069dfe03f7fccb5bda8d82d86790965b0119d6e8
xjgomez <jordi.aneas@bsc.es>
2017-12-21 14:58:20 +0100

    Fixed makefile.

--- src/common/environment.h
+++ src/common/environment.h
@@ -9,47 +9,63 @@
 
 #ifndef _EAR_CONFIGURATION_H_
 #define _EAR_CONFIGURATION_H_
-#include <ear_default_values.h>
 
-char * getenv_ear_tmp();
-char * getenv_ear_install_pathname();
-char * getenv_ear_db_pathname();
-char * getenv_ear_user_db_pathname();
-char * getenv_ear_gui_pathname();
-char * getenv_ear_coeff_db_pathname();
-char * getenv_ear_app_name();
-void set_ear_app_name(char *name);
-int getenv_ear_learning_phase();
-int getenv_ear_power_policy();
-double getenv_ear_power_policy_th();
-int getenv_ear_reset_freq();
+#define DEFAULT_LEARNING_PHASE          0
+#define DEFAULT_RESET_FREQ              0
+#define DEFAULT_VERBOSE                 0
+#define DEFAULT_DB_PATHNAME             ".ear_system_db"
+#define DEFAULT_USER_DB_PATHNAME        ".ear_user_db"
+#define DEFAULT_COEFF_PATHNAME          "COEFFICIENTS/COEFFS_"
+#define DEFAULT_POWER_POLICY            0
+#define DEFAULT_MIN_ENERGY_TH           0.1
+#define DEFAULT_MIN_TIME_TH             PERFORMANCE_GAIN
+#define DEFAULT_PERFORMANCE_ACURACY     1000000
+#define DEFAULT_VERBOSE                 0
+#define DEFAULT_MAX_P_STATE             1
+#define DEFAULT_MIN_P_STATE             EAR_MIN_P_STATE
+#define DEFAULT_P_STATE                 DEFAULT_MAX_P_STATE
+#define DYNAIS_TIMEOUT                  30
+#define MIN_ENERGY_TO_SOLUTION          0
+#define MIN_TIME_TO_SOLUTION            1
+#define MONITORING_ONLY                 2
+
 unsigned long getenv_ear_p_state();
 double getenv_ear_performance_accuracy();
+double getenv_ear_power_policy_th();
+void set_ear_app_name(char *name);
+void set_ear_tmp(char *new_tmp);
+char *getenv_ear_tmp();
+char *getenv_ear_install_pathname();
+char *getenv_ear_db_pathname();
+char *getenv_ear_user_db_pathname();
+char *getenv_ear_gui_pathname();
+char *getenv_ear_coeff_db_pathname();
+char *getenv_ear_app_name();
+int getenv_ear_learning_phase();
+int getenv_ear_power_policy();
+int getenv_ear_reset_freq();
 int getenv_ear_verbose();
 int getenv_ear_local_id();
 int getenv_ear_num_nodes();
 
-
-char * get_ear_tmp();
-void set_ear_tmp(char *new_tmp);
-char * get_ear_install_pathname();
-char * get_ear_db_pathname();
-char * get_ear_user_db_pathname();
-char * get_ear_gui_pathname();
-char * get_ear_coeff_db_pathname();
-char * get_ear_app_name();
-int get_ear_learning_phase();
-int get_ear_power_policy();
-double get_ear_power_policy_th();
-int get_ear_reset_freq();
 unsigned long get_ear_p_state();
 double get_ear_performance_accuracy();
-int get_ear_verbose();
+double get_ear_power_policy_th();
 void set_ear_verbose(int verb);
-int get_ear_num_nodes();
 void set_ear_total_processes(int procs);
+char *get_ear_tmp();
+char *get_ear_install_pathname();
+char *get_ear_db_pathname();
+char *get_ear_user_db_pathname();
+char *get_ear_gui_pathname();
+char *get_ear_coeff_db_pathname();
+char *get_ear_app_name();
+int get_ear_num_nodes();
+int get_ear_verbose();
+int get_ear_reset_freq();
+int get_ear_learning_phase();
 int get_ear_total_processes();
-
+int get_ear_power_policy();
 
 void ear_lib_environment();
 void ear_print_lib_environment();
