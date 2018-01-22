2c7ef736cf90c1e90c5dd0864d2780582e9f5bca
julitacorbalan <julita.corbalan@bsc.es>
2018-01-22 13:35:59 +0100

    compute from csv

--- src/tools/compute_projections_from_csv.c
+++ src/tools/compute_projections_from_csv.c
@@ -41,7 +41,7 @@ double time_proj(double time_f0,double cpi_proj,double cpi_f0,unsigned long f0,u
 }
 void usage(char *app)
 {
-	fprintf(stdout,"usage: %s coefficients_filename csv_file\n",app);
+	fprintf(stdout,"usage: %s coefficients_filename csv_file nominal\n",app);
 	exit(1);
 }
 
@@ -50,11 +50,14 @@ void main(int argc,char *argv[])
 	struct Coefficients_info *my_coeffs;
 	int fd,ret,size,p_states,i;
 	FILE *fd_apps;
+	char line[1024];
 	long f0;
 	double cpi_f0,tpi_f0,time_f0,power_f0,cpi_p;
 	struct App_info app_data;
 	if (argc!=4) usage(argv[0]);
 	f0=atoi(argv[3]);
+	fprintf(stderr,"Nominal %u\n",f0);
+	fprintf(stderr,"Reading coefficients %s\n",argv[1]);
 	fd=open(argv[1],O_RDONLY);
 	if (fd<0){
 		fprintf(stderr,"Coefficients file error: %s\n",strerror(errno));
@@ -76,32 +79,36 @@ void main(int argc,char *argv[])
 		fprintf(stderr,"Error when readin coefficients file (%s)\n",strerror(errno));
 		exit(1);
 	}
+	fprintf(stdout,"USERNAME;JOB_ID;NODENAME;APPNAME;AVG.FREQ;TIME;CPI;TPI;GBS;GFLOPS;DC-NODE-POWER;DRAM-POWER;PCK-POWER;DEF.FREQ;PROJECTION;POLICY_TH\n");
+	fprintf(stderr,"Reading metrics %s\n",argv[2]);
 	fd_apps=fopen(argv[2],"r");
 	if (fd_apps==NULL){
 		fprintf(stderr,"Error opening csv file %s (%s)\n",argv[2],strerror(errno));
 		exit(1);
 	}
-	fscanf(fd_apps,"%s\n");
-	while(fscanf(fd_apps,"%s;%s;%s;%s;%s;%f;%u;%u;%f;%f;%f;%f;%f;%f;%u;%llu;%llu;%f;%f;%f;%f;%f\n",
-	&app_data.job_id,&app_data.node_id,&app_data.user_id,&app_data.app_id,&app_data.policy,&app_data.th,
-	&app_data.f,&app_data.procs,
-	&app_data.seconds,&app_data.GIPS_f0,&app_data.GBS_f0,&app_data.POWER_f0,&app_data.TPI_f0,&app_data.CPI,
-	&app_data.nominal,&app_data.CYCLES,&app_data.INSTRUCTIONS,&app_data.POWER_DC,&app_data.DRAM_POWER,&app_data.PCK_POWER,
-	&app_data.Gflops,&app_data.EDP)){
+	fscanf(fd_apps,"%s\n",&line);
+	while((ret=fscanf(fd_apps,"%[^;];%[^;];%[^;];%[^;];%u;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%u;%[^;];%lf\n",
+	app_data.user_id,app_data.job_id,app_data.node_id,app_data.app_id,
+	&app_data.f,
+	&app_data.seconds,&app_data.CPI,&app_data.TPI_f0,&app_data.GBS_f0,&app_data.Gflops,&app_data.POWER_f0,
+	&app_data.DRAM_POWER,&app_data.PCK_POWER,&app_data.nominal,app_data.policy,&app_data.th))>0){
 	if (app_data.nominal==f0){
+	    fprintf(stderr,"Reference values for %s: avgf %u CPI %f TPI %f Time %f Power %f\n",
+		app_data.app_id,app_data.f,app_data.CPI,app_data.TPI_f0,app_data.seconds,app_data.POWER_f0);
 		cpi_f0=app_data.CPI;
 		tpi_f0=app_data.TPI_f0;
 		time_f0=app_data.seconds;
 		power_f0=app_data.POWER_f0;
-	    fprintf(stderr,"Reference values for %s: CPI %f TPI %f Time %f Power %f\n",app_data.app_id,cpi_f0,tpi_f0,time_f0,power_f0);
-/*
 	    for (i=0;i<p_states;i++){
 		    if (my_coeffs[i].available){
 			    cpi_p=cpi_proj(cpi_f0,tpi_f0,&my_coeffs[i]);
-			    fprintf(stdout,"%u;%f;%f;%f\n",my_coeffs[i].pstate,cpi_p,time_proj(time_f0,cpi_p,cpi_f0,f0,my_coeffs[i].pstate),power_proj(power_f0,tpi_f0,&my_coeffs[i]));
+			    fprintf(stdout,"%s;%s;%s;%s;%u;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%u;PROJECTION;0.0\n",
+				app_data.user_id,app_data.job_id,app_data.node_id,app_data.app_id,
+				my_coeffs[i].pstate,time_proj(time_f0,cpi_p,cpi_f0,f0,my_coeffs[i].pstate),
+				cpi_p,0.0,0.0,0.0,power_proj(power_f0,tpi_f0,&my_coeffs[i]),
+				0.0,0.0,f0);
 		    }
 	    }	
-*/
 	}
     }
 }
