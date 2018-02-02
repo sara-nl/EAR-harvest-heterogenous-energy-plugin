0c8750688cc76bc721b67f7b35927c4b2f69709c
xjgomez <jordi.aneas@bsc.es>
2018-02-02 10:39:35 +0100

    Fixed ear_models.c

--- src/library/ear_models/ear_models.c
+++ src/library/ear_models/ear_models.c
@@ -105,7 +105,7 @@ void init_power_models(unsigned int p_states,unsigned long *p_states_list)
 	sprintf(coeff_default_file, "%s.default", coeff_file);
 
 	// Coefficient pointers allocation
-	coefficients = (coefficient_t **) malloc(sizeof((coefficient_t *) * p_states);
+	coefficients = (coefficient_t **) malloc(sizeof(coefficient_t *) * p_states);
 
 	if (coefficients == NULL) {
 		ear_verbose(0, "EAR: Error allocating memory for p_states coefficients\n");
