c3ca8ba075563d34ddf416a4d3808b46f51de8ea
xjgomez <jordi.aneas@bsc.es>
2018-02-06 09:12:01 +0100

    Fixed types includes.

--- src/common/types/projection.c
+++ src/common/types/projection.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <states.h>
-#include "projection.h"
+#include <projection.h>
 
 #define PERMISSION S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
 #define OPTIONS O_WRONLY | O_CREAT | O_TRUNC | O_APPEND
