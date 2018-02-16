1cbb548bff11de80b48f9df2de6f0d8ee5b606ac
xjgomez <jordi.aneas@bsc.es>
2018-02-16 14:04:49 +0100

    Metrics are saved in a two size vectors.

--- src/library/ear_metrics/papi_generics.c
+++ src/library/ear_metrics/papi_generics.c
@@ -1,4 +1,14 @@
-//
-// Created by xgomez on 14/02/18.
-//
+void metrics_get_app_name(char *app_name)
+{
+	const PAPI_exe_info_t *prginfo = NULL;
 
+	PAPI_INIT_TEST(__FILE__);
+
+	if ((prginfo = PAPI_get_executable_info()) == NULL)
+	{
+		ear_verbose(0,"EAR(%s): Executable info not available. Exiting\n", __FILE__);
+		exit(2);
+	}
+
+	strcpy(app_name,prginfo->fullname);
+}
