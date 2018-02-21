861254469e6a30442531f86c4d6b7f23bccb4e7a
xjgomez <jordi.aneas@bsc.es>
2018-02-21 16:38:49 +0100

    Fixed makefile.

--- src/metrics/papi/generics.h
+++ src/metrics/papi/generics.h
@@ -1,18 +1,25 @@
 #ifndef EAR_PAPI_MACROS_H
 #define EAR_PAPI_MACROS_H
 
-#define PAPI_INIT_TEST(file)                                        			\
-    if (PAPI_is_initialized() == PAPI_NOT_INITED) {								\
-        if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT) {			\
-            ear_verbose(0, "%s: ERROR, intializing the PAPI library, exiting\n", file);    \
-            exit(1);															\
-        }																		\
+#define PAPI_INIT_TEST(file) \
+    int papi_init_test; \
+    if (PAPI_is_initialized() == PAPI_NOT_INITED) \
+	{ \
+	papi_init_test = PAPI_library_init(PAPI_VER_CURRENT); \
+	ear_verbose(0, "%d - %d\n", PAPI_VER_CURRENT, papi_init_test); \
+        \
+	if (papi_init_test != PAPI_VER_CURRENT) \
+	{ \
+            ear_verbose(0, "%s: ERROR intializing the PAPI library (%s), exiting\n", \
+		file, PAPI_strerror(papi_init_test)); \
+            exit(1); \
+        } \
     }
 
-#define PAPI_INIT_MULTIPLEX_TEST(file)											\
-    int papi_multi_test;														\
-    if ((papi_multi_test = PAPI_multiplex_init()) != PAPI_OK) {					\
-        ear_verbose(0,"%s: WARNING, %s\n", file, PAPI_strerror(papi_multi_test));	\
+#define PAPI_INIT_MULTIPLEX_TEST(file) \
+	int papi_multi_test; \
+	if ((papi_multi_test = PAPI_multiplex_init()) != PAPI_OK) { \
+		ear_verbose(0,"%s: WARNING, %s\n", file, PAPI_strerror(papi_multi_test)); \
 	}
 
 //
