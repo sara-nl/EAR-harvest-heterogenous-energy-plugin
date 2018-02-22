f39d5142981f7089a9344c63b60042c7205aab3e
julita.corbalan <julita.corbalan@bsc.es>
2018-02-22 18:51:51 +0100

    multiplr processes

--- src/library/common/externs.h
+++ src/library/common/externs.h
@@ -27,6 +27,8 @@ extern int ear_use_turbo;
 extern int ear_whole_app;
 extern int ear_papi_init;
 extern int ear_my_rank;
+extern int ear_my_local_id;
+extern int ear_my_rank_size;
 extern int report;
 
 #else
--- src/library/ear_frequency/ear_cpufreq.c
+++ src/library/ear_frequency/ear_cpufreq.c
@@ -173,8 +173,12 @@ unsigned long ear_cpufreq_set(unsigned int cpuid,unsigned long newfreq)
 	if (ear_is_valid_frequency(newfreq)){
 		ear_cpufreq[cpuid]=newfreq;
 		// cpufreq_set_frequency(cpuid,newfreq);
-		ret=ear_daemon_client_change_freq(newfreq);
-		return ret;
+		if (!ear_my_local_id){ 
+			ret=ear_daemon_client_change_freq(newfreq);
+			return ret;
+		}else{
+			return ret;
+		}
 	}else return 0;
 
 }
@@ -182,7 +186,8 @@ unsigned long ear_cpufreq_set(unsigned int cpuid,unsigned long newfreq)
 unsigned long ear_cpufreq_set_node(unsigned long newfreq)
 {
 	ear_verbose(4,"EAR: ear_cpufreq_set_node newfreq=%u\n", newfreq);
-	ear_frequency = ear_daemon_client_change_freq(newfreq);
+	if (!ear_my_local_id) ear_frequency = ear_daemon_client_change_freq(newfreq);
+	else ear_frequency=newfreq;
 	return ear_frequency;
 }
 
