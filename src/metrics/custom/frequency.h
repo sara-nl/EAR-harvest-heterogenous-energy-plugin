970096410f1132619da8bb99733c6ec2eaaa9e29
xjgomez <jordi.aneas@bsc.es>
2018-03-09 08:44:17 +0100

    Fixed type size from uncore.

--- src/metrics/custom/frequency.h
+++ src/metrics/custom/frequency.h
@@ -9,21 +9,20 @@
 
 #define MSR_IA32_APERF 0x000000E8
 #define MSR_IA32_MPERF 0x000000E7
-#define EAPERF -1
 
 // This functions are used to report the avg. freq for the whole app.
 int aperf_get_global_avg_frequency_init(unsigned int cpu);
 int aperf_get_global_avg_frequency_end(unsigned int cpu, unsigned long *frequency);
 
-void aperf_get_avg_frequency_init_all_cpus(unsigned int num_cpus);
-void aperf_get_global_avg_frequency_init_all_cpus(unsigned int num_cpus);
+void aperf_get_avg_frequency_init_all_cpus();
+void aperf_get_global_avg_frequency_init_all_cpus();
 
 // These functions are used during the execution
 int aperf_get_avg_frequency_init(unsigned int cpu);
 int aperf_get_avg_frequency_end(unsigned int cpu, unsigned long *frequency);
 
-unsigned long aperf_get_avg_frequency_end_all_cpus(unsigned int num_cpus);
-unsigned long aperf_get_global_avg_frequency_end_all_cpus(unsigned int num_cpus);
+unsigned long aperf_get_avg_frequency_end_all_cpus();
+unsigned long aperf_get_global_avg_frequency_end_all_cpus();
 
 // Init & dispose functions
 int aperf_init(unsigned int num_cpus);
