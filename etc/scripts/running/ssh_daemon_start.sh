e4d745ab62b147e089bd2d647fa0eb28508af00f
xjgomez <jordi.aneas@bsc.es>
2018-03-14 17:16:23 +0100

    Perfected ear environment module.

--- etc/scripts/running/ssh_daemon_start.sh
+++ etc/scripts/running/ssh_daemon_start.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
-if [[ $# -ne 2 ]]
+if [[ $# -ne 3 ]]
 then
 	echo -e "Usage: ssh_daemon_start.sh hostlist p_state"
 	echo -e "\thostlist: a host name list file, a host name list or 'local'"
 	echo -e "\tp_state: the maximum P_STATE which the daemon can set of the whole range"
+	echo -e "\tdb_pathname: the path where de job DBs will be saved"
 	exit 1
 fi
 
@@ -19,8 +20,6 @@ then
 else
 	export HOSTLIST="$(echo $(cat $1))"
 fi
-
-module load ear
 
 if [ -z $EAR_INSTALL_PATH ]
 then
@@ -37,10 +36,10 @@ do
 	then
 		mkdir -p ${EAR_TMP}
 		sudo LD_LIBRARY_PATH="${FREEIPMI_LIB_PATH}:${PAPI_LIB_PATH}:${CPUPOWER_LIB_PATH}:${LD_LIBRARY_PATH}" \
-                EAR_DB_PATHNAME=${EAR_DB_PATHNAME} $EAR_INSTALL_PATH/sbin/eard $2 ${EAR_TMP} ${EAR_VERBOSE} &
+                EAR_DB_PATHNAME=${3} $EAR_INSTALL_PATH/sbin/eard $2 ${EAR_TMP} ${EAR_VERBOSE} &
 	else
 		ssh ${i} mkdir -p ${EAR_TMP}
 	 	ssh ${i} sudo LD_LIBRARY_PATH="${FREEIPMI_LIB_PATH}:${PAPI_LIB_PATH}:${CPUPOWER_LIB_PATH}:${LD_LIBRARY_PATH}" \
-        	EAR_DB_PATHNAME=${EAR_DB_PATHNAME} $EAR_INSTALL_PATH/sbin/eard $2 ${EAR_TMP} ${EAR_VERBOSE} &
+        	EAR_DB_PATHNAME=${3} $EAR_INSTALL_PATH/sbin/eard $2 ${EAR_TMP} ${EAR_VERBOSE} &
 	fi
 done
