94a4211c06cafbcf0b224be9f07623fe8d36f93f
julitacorbalan <julita.corbalan@bsc.es>
2018-04-13 18:54:44 +0200

    NEW_ITERATION messages with verbose 3

--- src/library/mpi_intercept/ear_api.c
+++ src/library/mpi_intercept/ear_api.c
@@ -357,7 +357,7 @@ void ear_mpi_call(mpi_call call_type, p2i buf, p2i dest)
 
 			if (loop_with_signature)
 			{
-				ear_verbose(1,"NEW_ITERATION level %u event %u size %u iterations %u\n",
+				ear_verbose(3,"NEW_ITERATION level %u event %u size %u iterations %u\n",
 					ear_level, ear_event, ear_loop_size, ear_iterations);
 			}
 
