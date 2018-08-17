72184dfcf1b8728f8177c48452330896d794dc35
julitacorbalan <julita.corbalan@bsc.es>
2018-08-17 11:22:38 +0200

    rapl test fixed

--- src/tests/check/papi_comp_enabled.c
+++ src/tests/check/papi_comp_enabled.c
@@ -27,19 +27,25 @@
 *	The GNU LEsser General Public License is contained in the file COPYING	
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <papi.h>
 
 int main(int argc, char *argv[])
 {
-    const PAPI_component_info_t *info;
+    const PAPI_component_info_t *info=NULL;
     int state = PAPI_library_init(PAPI_VER_CURRENT);
     assert(state == PAPI_VER_CURRENT);
 
     state = PAPI_get_component_index(argv[1]);
-    assert(state == PAPI_OK);
+
+    assert(state >= PAPI_OK);
+	
 
     info = PAPI_get_component_info(state);
+
+	assert(info!=NULL);
     assert(!info->disabled);
 
     return 0;
