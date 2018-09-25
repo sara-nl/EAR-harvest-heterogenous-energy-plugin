7f0917cceec2c1a082edc7cea44b6b0e2a6cc8c2
lluisalonso <lluis.alonso@bsc.es>
2018-09-25 16:50:19 +0200

    get_app_list now accepts a node_name by parameter, always retrieves from learning_database.

--- src/common/database/db_helper.c
+++ src/common/database/db_helper.c
@@ -734,10 +734,12 @@ int db_read_applications(application_t **apps,uint is_learning, int max_apps, ch
 
     char query[512];
     if (is_learning && node_name != NULL)
-        sprintf(query,  "SELECT Learning_applications.* FROM Learning_applications INNER JOIN "\
+//        sprintf(query,  "SELECT Learning_applications.* FROM Learning_applications INNER JOIN "\
                         "Learning_jobs ON job_id = id where job_id < (SELECT max(id) FROM (SELECT (id) FROM "\
                         "Learning_jobs WHERE id > %d ORDER BY id asc limit %u) as t1)+1 and "\
                         "job_id > %d AND node_id='%s' GROUP BY job_id, step_id", current_job_id, max_apps, current_job_id, node_name);
+        sprintf(query,  "SELECT Learning_applications.* FROM Learning_applications WHERE job_id > %d AND node_id='%s' ORDER BY job_id "\
+                        "LIMIT %d", current_job_id, node_name, max_apps);
     else if (is_learning && node_name == NULL)
         sprintf(query,  "SELECT Learning_applications.* FROM Learning_applications INNER JOIN "\
                         "Learning_jobs ON job_id = id where job_id < (SELECT max(id) FROM (SELECT (id) FROM "\
@@ -754,6 +756,7 @@ int db_read_applications(application_t **apps,uint is_learning, int max_apps, ch
                         "Jobs WHERE id > %d ORDER BY id asc limit %u) as t1)+1 and "\
                         "job_id > %d GROUP BY job_id, step_id", current_job_id, max_apps, current_job_id);
 
+    fprintf(stderr, "QUERY: %s\n", query);
    	num_apps = mysql_retrieve_applications(connection, query, apps, is_learning);
    
   	if (num_apps == EAR_MYSQL_ERROR){
