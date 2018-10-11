97680e44705012e881fd5d4d2d8f56232afd7e75
xjgomez <jordi.aneas@bsc.es>
2018-10-11 09:44:06 +0200

    Improved m4 and scripts.

---
+++ etc/scripts/learning/learning_phase_coeffs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+        echo -e "Usage: hostlist"
+        echo -e "\thostlist: a host name list file"
+        exit 1
+fi
+
+if [ ! -f $1 ]
+then
+    echo "Error, this nodelist file doesn't exists"
+fi
+
+if [ -z $EAR_INSTALL_PATH ]
+then
+    echo -e "ERROR: EAR_INSTALL_PATH environment variable is not set."
+    echo -e "TIP! Load the EAR environment module."
+    exit 1
+fi
+
+# Edit minimum frequency (in KHz)
+export MIN_FREQUENCY=1900000
+
+# Edit output options
+export OUT_COE="$HOME/coe."
+
+# Non-edit region
+export HOSTLIST="$(echo $(cat $1))"
+
+for i in ${HOSTLIST}
+do
+    echo "Computing coefficients for node=${i}"
+	$EAR_INSTALL_PATH/coeffs_compute $OUT_COE $MIN_FREQUENCY ${i}
+done
--- etc/scripts/learning/learning_phase_execute.sh
+++ etc/scripts/learning/learning_phase_execute.sh
@@ -3,13 +3,13 @@
 if [[ $# -ne 1 ]]
 then
         echo -e "Usage: hostlist"
-        echo -e "\thostlist: a host name list file"
+        echo -e "    hostlist: a hostname list of the nodes"
         exit 1
 fi
 
 if [ ! -f $1 ]
 then
-    echo "Error, this nodelist file doesn't exists"
+    echo "Error, thie nodelist file doesn't exists"
 fi
 
 if [ -z $EAR_INSTALL_PATH ]
@@ -20,26 +20,26 @@ then
 fi
 
 # Edit architecture values
-export CORES=16
+export CORES=40
 export SOCKETS=2
-export CORES_PER_SOCKET=8
+export CORES_PER_SOCKET=20
 
 # Edit learning phase parameters
 export EAR_MIN_P_STATE=1
 export EAR_MAX_P_STATE=6
 export EAR_TIMES=3
 
-# Edit output files
-export ERR = $HOME/lp_err
-export OUT = $HOME/lp_out
+# Edit output options
+export OUT_OUT="$HOME/out."
+export OUT_ERR="$HOME/err."
 
 # Non-edit region
 export HOSTLIST="$(echo $(cat $1))"
-export EAR_LEARNING_PHASE=1
 export BENCHS_MODE="test"
 
 for i in ${HOSTLIST}
 do
-    echo "Executing learning phase in node in node=${i}"
-	sbatch -w ${i} -N 1 -n 1 --exclusive -o $ERR.${i}.txt -e $OUT.${i}.txt $EAR_INSTALL_PATH/bin/scripts/learning/helpers/kernels_iterator.sh
+	echo "Executing learning phase in node=${i}"
+	sbatch -w ${i} -N 1 -n 1 --exclusive -o $OUT_OUT/${i} -e $OUT_ERR/${i} \
+		$EAR_INSTALL_PATH/bin/scripts/learning/helpers/kernels_iterator.sh
 done
