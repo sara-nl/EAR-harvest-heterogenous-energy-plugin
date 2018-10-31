6a6b3caf00f06776feebe2dca593524248658f86
xgomez <jordi.aneas@bsc.es>
2018-10-31 07:46:50 +0100

    Added tools scripts and README.md.

---
+++ etc/scripts/tools/coeffs_quality.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+        echo -e "Usage: hostlist"
+        echo -e "\thostlist: a host name list file"
+        exit 1
+fi
+
+if [ ! -f $1 ]
+then
+    echo -e "Error, the nodelist file doesn't exists"
+	exit -1
+fi
+
+if [ -z $EAR_INSTALL_PATH ]
+then
+    echo -e "ERROR: EAR_INSTALL_PATH environment variable is not set."
+    echo -e "TIP! Load the EAR environment module."
+    exit 1
+fi
+
+#
+export HOSTLIST="$(echo $(cat $1))"
+
+#
+for i in ${HOSTLIST}
+do
+	/home/xjaneas/bench/tools/coeffs_quality ${i} 2100000 -G
+done
