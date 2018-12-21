5ad5b5d9bdb4e41f78f79e7450cc99bd312ba82f
xjgomez <jordi.aneas@bsc.es>
2018-12-21 08:59:05 +0100

    Added output headers proposal.

---
+++ src/common/output/verbose.h
@@ -0,0 +1,19 @@
+#ifndef EAR_VERBOSE_H
+#define EAR_VERBOSE_H
+
+#include <stdio.h>
+#include <string.h>
+
+int verb_channel	__attribute__((weak));
+int verb_level		__attribute__((weak));
+
+#define verbose(v, ...) \
+	if (v <= verb_level) { \
+        dprintf(verb_channel, __VA_ARGS__); \
+        dprintf(verb_channel, "\n"); \
+	}
+
+#define verb(v, ...) \
+	verbose(v, __VA_ARGS__)
+
+#endif //EAR_VERBOSE_H
