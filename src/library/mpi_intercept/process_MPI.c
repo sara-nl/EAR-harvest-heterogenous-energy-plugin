30d59023675b9b32b0b443fd352971fdbb76cd60
julitacorbalan <julita.corbalan@bsc.es>
2019-05-28 08:41:52 +0200

    fixing minor error for policy plugin

--- src/library/mpi_intercept/process_MPI.c
+++ src/library/mpi_intercept/process_MPI.c
@@ -59,6 +59,7 @@ void before_mpi(mpi_call call_type, p2i buf, p2i dest) {
 	begin_in_mpi=PAPI_get_real_usec();
 	#endif
   #if USE_POLICY_PLUGIN
+	int ret;
 	if (power_policy.new_mpi_call!=NULL){
 		if (power_policy.new_mpi_call(call_type,buf,dest)!=EAR_SUCCESS) error("Error in power_policy.new_mpi_call %d",ret);
 	}
@@ -67,6 +68,7 @@ void before_mpi(mpi_call call_type, p2i buf, p2i dest) {
 
 void after_mpi(mpi_call call_type){
   #if USE_POLICY_PLUGIN
+	int ret;
 	if (power_policy.end_mpi_call!=NULL){
 		if (power_policy.end_mpi_call(call_type)!=EAR_SUCCESS) error("Error in power_policy.end_mpi_call %d",ret);
 	}
