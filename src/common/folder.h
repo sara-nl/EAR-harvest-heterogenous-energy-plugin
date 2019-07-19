4bf66675ced1ca93a28a83ba5c87011ba9c3d93f
xjgomez <jordi.aneas@bsc.es>
2019-07-19 17:43:57 +0200

    Added folder class.

--- src/common/folder.h
+++ src/common/folder.h
@@ -27,66 +27,23 @@
 *   The GNU LEsser General Public License is contained in the file COPYING
 */
 
-#ifndef _FILE_H
-#define _FILE_H
+#ifndef EAR_PRIVATE_FOLDER_H
+#define EAR_PRIVATE_FOLDER_H
 
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <common/sizes.h>
 #include <common/states.h>
 
-#define FC_MAX_OBJECTS	10
-#define F_WR 			O_WRONLY
-#define F_RD			O_RDONLY
-#define F_CR 			O_CREAT
-#define F_TR			O_TRUNC
-#define F_UR 			S_IRUSR
-#define F_UW 			S_IWUSR
-#define F_UX 			S_IXUSR
-#define F_GR 			S_IRGRP
-#define F_GW 			S_IWGRP
-#define F_GX 			S_IXGRP
-#define F_OR 			S_IROTH
-#define F_OW 			S_IWOTH
-#define F_OX 			S_IXOTH
+typedef struct folder {
+	char file_name[SZ_NAME_LARGE];
+	DIR *dir;
+} folder_t;
 
-/** Locks the file for writting in a fixe dpart */
-int file_lock(int fd);
+state_t folder_open(folder_t *folder, char *path);
 
-/** Creates a file to be used  as lock. It doesn't locks the file */
-int file_lock_create(char *lock_file_name);
+state_t folder_close(folder_t *folder);
 
-/** Opens a file with O_EXCL, only one process per node will do that */
-int file_lock_master(char *lock_file_name);
+char *folder_getnext(folder_t *folder, char *prefix, char *suffix);
 
-/** Closes and removes the lock file */
-void file_lock_clean(int fd,char *lock_file_name);
-
-/** Unlocks the file */
-int file_unlock(int fd);
-
-/** Releases a lock file */
-void file_unlock_master(int fd,char *lock_file_name);
-
-/** */
-int file_is_regular(const char *path);
-
-/** */
-int file_is_directory(const char *path);
-
-/** */
-ssize_t file_size(char *path);
-
-/** */
-state_t file_read(const char *path, char *buffer, size_t size);
-
-/** */
-state_t file_write(const char *path, const char *buffer, size_t size);
-
-/** */
-state_t file_clean(const char *path);
-
-#endif
+#endif //EAR_PRIVATE_FOLDER_H
