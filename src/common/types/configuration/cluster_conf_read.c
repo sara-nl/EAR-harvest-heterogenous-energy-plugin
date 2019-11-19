d7a3c0913cf5f4037b53d2716f9f45d83ced9cbd
lluisalonso <lluis.alonso@bsc.es>
2019-11-19 16:33:46 +0100

    Fixed an error where memory would be zeroed after setting the default values in cluster_conf, not before

--- src/common/types/configuration/cluster_conf_read.c
+++ src/common/types/configuration/cluster_conf_read.c
@@ -211,7 +211,6 @@ static void generate_node_ranges(node_island_t *island, char *nodelist)
 
 void get_cluster_config(FILE *conf_file, cluster_conf_t *conf)
 {
-	memset(conf, 0, sizeof(cluster_conf_t));
 	char line[256];
     char def_policy[128];
 	char *token;
@@ -1097,6 +1096,7 @@ int read_cluster_conf(char *conf_path,cluster_conf_t *my_conf)
 		error("ERROR opening file: %s\n", conf_path);
 		return EAR_ERROR;
 	}
+	memset(&my_conf, 0, sizeof(cluster_conf_t));
 	set_ear_conf_default(my_conf);
 	get_cluster_config(conf_file, my_conf);
     if ((my_conf->num_policies < 1) || (my_conf->num_islands < 1) || (my_conf->default_policy >TOTAL_POLICIES ))
