4edb88085c732722cc7216ae7c898eac0cadd992
xjgomez <jordi.aneas@bsc.es>
2019-11-28 16:37:39 +0100

    Fixed SLURM plugin.

--- src/slurm_plugin/slurm_plugin_rcom_eard.c
+++ src/slurm_plugin/slurm_plugin_rcom_eard.c
@@ -50,8 +50,7 @@ static int plug_rcom_eard(spank_t sp, plug_serialization_t *sd, int new_job)
 
 		if (new_job) {
 			eards_new_job(&sd->job.app);
-			plug_verbose(sp, 3, "application after new job:");
-			plug_print_application(&sd->job.app);
+			plug_print_application(sp, &sd->job.app);
 		} else {
 			eards_end_job(sd->job.app.job.id, sd->job.app.job.step_id);
 		}
