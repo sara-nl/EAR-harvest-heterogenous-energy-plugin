ca4c1808cae18caa444f78532118439bb7f1e125
xjgomez <jordi.aneas@bsc.es>
2019-12-20 11:25:49 +0100

    Updated ERUN with STEP_ID locking method.

--- src/slurm_plugin/erun.c
+++ src/slurm_plugin/erun.c
@@ -4,11 +4,13 @@
 #include <slurm_plugin/slurm_plugin_serialization.h>
 
 //
+extern plug_serialization_t sd;
+
+// Buffers
 char plug_tmp[SZ_NAME_LARGE];
 char plug_etc[SZ_NAME_LARGE];
 char plug_pfx[SZ_NAME_LARGE];
 char plug_def[SZ_NAME_LARGE];
-char path_lock[SZ_PATH];
 char path_prog[SZ_PATH];
 char buffer[SZ_PATH];
 
@@ -17,189 +19,47 @@ char **_argv;
 int    _argc;
 
 //
-extern plug_serialization_t sd;
+int _inactive;
+int _clean;
+int _help;
 
 //
-int   _master;
-int   _fd;
-int   _sp;
-int   _at;
+int _master;
+int _step;
+int _job;
+int _sp;
+int _at;
 
-spank_err_t spank_getenv (spank_t spank, const char *var, char *buf, int len)
-{
-	char *c;
-
-	if (var == NULL || buf == NULL) {
-		return ESPANK_ERROR;
-	}
-	if ((c = getenv(var)) == NULL) {
-		return ESPANK_ERROR;
-	}
-
-	snprintf(buf, len, "%s", c);
-	return ESPANK_SUCCESS;
-}
-
-spank_err_t spank_setenv (spank_t spank, const char *var, const char *val, int overwrite)
-{
-	if (var == NULL || val == NULL) {
-		return ESPANK_ERROR;
-	}
-	if (setenv (var, val, overwrite) == -1) {
-		return ESPANK_ERROR;
-	}
-	return ESPANK_SUCCESS;
-}
-
-spank_err_t spank_unsetenv (spank_t spank, const char *var)
-{
-	if (var == NULL) {
-		return ESPANK_ERROR;
-	}
-	return unsetenv(var) == 0;
-}
-
-int plug_action_is(int _ac, int action)
+int plug_is_action(int _ac, int action)
 {
 	return _ac == action;
 }
 
-spank_context_t spank_context (void)
+int plug_is_enabled(int argc, char *argv[])
 {
-	return _sp;
-}
-
-spank_err_t spank_get_item (spank_t spank, spank_item_t item, int *p)
-{
-	if (item == S_JOB_ID) {
-		*p = atoi(getenv("SLURM_JOB_ID"));
-	} else if (item == S_JOB_STEPID) {
-		*p = atoi(getenv("SLURM_STEP_ID"));
-	} else {
-		*p = 0;
-	}
-	return ESPANK_SUCCESS;
-}
-
-char *slurm_hostlist_shift (hostlist_t host_list)
-{
-	static int count = 1;
-        
-	if (count == 1) {
-                count = 0;
-                return host_list;
-        } else {
-		count = 1;
-                return NULL;
-        }
-}
-
-hostlist_t slurm_hostlist_create (char *node_list)
-{
-	char *copy = malloc(strlen(node_list));
-	return strcpy(copy, node_list);
-}
-
-spank_err_t spank_option_register_print(spank_t sp, struct spank_option *opt)
-{
-	int n = strlen(opt->usage);
-	int i = 0;
-	int o = 0;
-	char c;
-
-	printf("\t--%s\t\t", opt->name);
-	if (strlen(opt->name) < 5) {
-		printf("\t");
-	}
-	if (n < 64) {
-		printf("%s\n", opt->usage);
-		return ESPANK_SUCCESS;
-	}
-	while (i < n)
-	{
-		c = opt->usage[i];
-		printf("%c", c);
-		o = (o == 1) | (i != 0 && (i % 64) == 0);
-		if (o && (c == ' ' || c == ',')) {
-			printf("\n\t\t\t\t");
-			o = 0;
-		}
-		i += 1;
-	}
-	printf("\n");
-
-	return ESPANK_SUCCESS;
-}
-
-spank_err_t spank_option_register_call(int argc, char *argv[], spank_t sp, struct spank_option *opt)
-{
-	char *p;
-
-	sprintf(buffer, "--%s=", opt->name);
-	p = plug_acav_get(argc, argv, buffer);
-	
-	if (p == NULL) {
-		return ESPANK_SUCCESS;
-	} if (strlen(p) == 0) {
-		return ESPANK_SUCCESS;
-	}
-
-	opt->cb(0, p, 0);
-	
-	return ESPANK_SUCCESS;
-}
-
-spank_err_t spank_option_register(spank_t sp, struct spank_option *opt)
-{
-	if (plug_context_is(sp, Context.error)) {
-		return spank_option_register_print(sp, opt);
-	} else if (plug_context_is(sp, Context.srun)) {
-		return spank_option_register_call(_argc, _argv, sp, opt);
-	}
-	return ESPANK_SUCCESS;
+	return getenv(Var.context.rem) != NULL;
 }
 
 int help(int argc, char *argv[])
 {
 	printf("Usage: %s [OPTIONS]\n", argv[0]);
 	printf("\nOptions:\n");
+	printf("\t--job-id=<arg>\tSet the JOB_ID.\n");
 	printf("\t--nodes=<arg>\t\tSets the number of nodes.\n");
 	printf("\t--program=<arg>\t\tSets the program to run.\n");
 	printf("\t--plugstack [ARGS]\tSet the SLURM's plugstack arguments. I.e:\n");
 	printf("\t\t\t\t--plugstack prefix=/hpc/opt/ear default=on...\n");
+	printf("\t--clean\t\tCleans the internal files.\n");
 	printf("SLURM options:\n");
 
 	return 0;
 }
 
-int lock(int argc, char *argv[])
+int clean(int argc, char *argv[])
 {
-	sprintf(path_lock, "/tmp/earplug.lock");
-
-	_fd = file_lock_master(path_lock);
-
-	if (_fd > 0) {
-		return 1;
-	}
-	
-	_fd = 0;
+	plug_verbose(0, "cleaning temporal folder");
+	system("/tmp/erun.*.lock");
 	return 0;
-}
-
-int spinlock(int argc, char *argv[])
-{
-	while (access(path_lock, F_OK) == 0);
-}
-
-int unlock(int argc, char *argv[])
-{
-	if (_fd == 0) {
-		return 0;
-	}
-	
-	file_unlock_master(_fd, path_lock);
-	
-	return 1;
 }
 
 int fake_slurm_spank_user_init(spank_t sp, int ac, char **av)
@@ -233,7 +93,7 @@ int pipeline(int argc, char *argv[], int sp, int at)
 	_sp = sp;
 	_at = at;
 
-	if (plug_action_is(_at, Action.init))
+	if (plug_is_action(_at, Action.init))
 	{
 		slurm_spank_init(_sp, argc, argv);
 
@@ -253,7 +113,7 @@ int pipeline(int argc, char *argv[], int sp, int at)
 
 		}
 	}
-	else if(plug_action_is(_at, Action.exit))
+	else if(plug_is_action(_at, Action.exit))
 	{
 		if (plug_context_is(_sp, Context.remote))
 		{
@@ -290,18 +150,22 @@ int job(int argc, char *argv[])
 {
 	char *p = NULL;
 	int i = 0;
-	int h = 0;
 
 	// Input parameters initial
 	print_argv(argc, argv);
 
+	// Clean
+	for (i = 0; i < argc; ++i) {
+		if ((strlen(argv[i]) >= 7) && (strncmp("--clean", argv[i], 7) == 0)) {
+			_clean = 1;
+		}
+	}
 	// Help?
 	for (i = 0; i < argc; ++i) {
 		if ((strlen(argv[i]) >= 6) && (strncmp("--help", argv[i], 6) == 0)) {
-			h = 1;
+			_help = 1;
 		}
 	}
-
 	// Setting the program and the job name
 	for (i = 0; i < argc; ++i) {
 		if ((strlen(argv[i]) > 8) && (strncmp("--program=", argv[i], 10) == 0)) {
@@ -314,7 +178,7 @@ int job(int argc, char *argv[])
 		sprintf(path_prog, "%s", p);
 		setenv("SLURM_JOB_NAME", p, 1);
 	} else {
-		h = 1;
+		_helph = !_clean;
 	}
 
 	// Converting configuration enrivonment variables 
@@ -346,15 +210,6 @@ int job(int argc, char *argv[])
 		_argv[i] = argv[i];
 	}
 
-	// Getting the job id (it cant be created,
-	// depends on the queue manager:
-	if ((p = getenv("SLURM_JOB_ID")) == NULL) {
-		setenv("SLURM_JOB_ID", "0", 1);
-	}
-	if (getenv("SLURM_STEP_ID") == NULL) {
-		setenv("SLURM_STEP_ID", "0", 1);
-	}
-
 	// Getting the number of nodes
 	for (i = 0; i < argc; ++i) {
 		if ((strlen(argv[i]) > 7) && (strncmp("--nodes=", argv[i], 8) == 0)) {
@@ -382,11 +237,30 @@ int job(int argc, char *argv[])
 		setenv("SLURM_STEP_NUM_NODES", nnodes, 1);
 	}
 
+	// Getting the job id (it cant be created,
+	// depends on the queue manager:
+	if ((p = getenv("SLURM_JOB_ID")) == NULL) {
+		setenv("SLURM_JOB_ID", "0", 1);
+	} else {
+		_job = atoi(p);
+	}
+	if ((p = getenv("SLURM_STEP_ID")) == NULL) {
+		setenv("SLURM_STEP_ID", "0", 1);
+	} else {
+		_step = atoi(p);
+	}
+
 	// Input parameters final
 	print_argv(_argc, _argv);
 	plug_verbose(_sp, 3, "program: '%s'", path_prog);
 
-	return h;
+	return 0;
+}
+
+int step(int argc, char *argv[], int step_id)
+{
+	sprintf(buffer, "%d", step_id);
+	setenv("SLURM_STEP_ID", buffer, 1);
 }
 
 int execute(int argc, char *argv[])
@@ -395,16 +269,18 @@ int execute(int argc, char *argv[])
 	return 0;
 }
 
-int plugin_enabled(int argc, char *argv[])
-{
-	return getenv(Var.context.rem) != NULL;
-}
-
 int main(int argc, char *argv[])
 {
-	// Arguments reading
-	if (job(argc, argv))
-	{
+	// Creating job and reading arguments
+	job(argc, argv);
+
+	// Clean
+	if (_clean) {
+		return clean();
+	}
+
+	// Help
+	if (_help) {
 		help(_argc, _argv);
 		// No context initializations
 		pipeline(_argc, _argv, Context.error, Action.init);
@@ -414,21 +290,27 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	// In case the plugin was enabled, just execute
-	if (plugin_enabled(_argc, _argv)) {
+	// Inactive
+	if (_inactive) {
 		return execute(_argc, _argv);
 	}
 
-	// The master simulation
-	if (lock(_argc, _argv)) {
-fprintf(stderr, "MASTER\n");
+	// Simulating the single pipeline
+	if ((_master = lock_master(_argc, _argv)))
+	{
 		plug_verbose(_sp, 2, "got the lock file");
-		_master = 1;
+
+		_step = lock_step(_argc, _argv, _job, _step);
 	} else {
-fprintf(stderr, "NO MASTER\n");
+		plug_verbose(_sp, 4, "missed the lock file, spinlock");
 		plug_verbosity_silence(_sp);
-		spinlock(_argc, _argv);
+
+		//
+		_step = spinlock_step(_argc, _argv);
 	}
+
+	// Creating step
+	step(_argc, _argv, _step);
 
 	// Local context initialization
 	pipeline(_argc, _argv, Context.srun, Action.init);
@@ -436,20 +318,19 @@ fprintf(stderr, "NO MASTER\n");
 	pipeline(_argc, _argv, Context.remote, Action.init);
 
 	if (_master) {
-		sleep(1);
-		unlock(_argc, _argv);
+		unlock_step(_argc, _argv, _step);
 	}
 
 	execute(_argc, _argv);
-
-	if (_master) {
-		sleep(2);
-	}
 
 	// Remote context finalization
 	pipeline(_argc, _argv, Context.remote, Action.exit);
 	// Local context finalization
 	pipeline(_argc, _argv, Context.srun, Action.exit);
-	
+
+	if (_master) {
+		lock_clean(_argc, _argv);
+	}
+
 	return 0;
 }
