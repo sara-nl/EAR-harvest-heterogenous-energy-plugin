db39cbc7704222bde59039b22b7357228d95a2ce
xgomez <jordi.aneas@bsc.es>
2020-08-25 16:19:12 +0200

    Fixed loader.c when loading concrete libraries if does not have MPI symbols.

--- src/common/system/symplug.c
+++ src/common/system/symplug.c
@@ -19,6 +19,18 @@
 #include <common/includes.h>
 #include <common/system/symplug.h>
 
+state_t symplug_test(void *calls[], uint n)
+{
+	uint i;
+	for (i = 0; i < n; ++i)
+	{
+		if (calls[i] == NULL) {
+			return EAR_ERROR;
+		}
+	}
+	return EAR_SUCCESS;
+}
+
 state_t symplug_join(void *handle, void *calls[], const char *names[], uint n)
 {
 	char *error;
