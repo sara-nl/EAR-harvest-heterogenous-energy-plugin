02f086603c97ca14a32ef7da47e13c2912813851
xjgomez <jordi.aneas@bsc.es>
2020-12-15 12:38:39 +0100

    Added new indications in connector.h

--- src/management/cpufreq/connector.h
+++ src/management/cpufreq/connector.h
@@ -32,52 +32,52 @@ typedef struct governor{
 state_t frequency_init(uint x);
 state_t frequency_dispose();
 
-// Virtualizable. Init required. | Not used without initialization. | EARD, EARL, DVFS.
+// Virtualizable. Init required. | Not used without initialization. | EARD, EARL, DVFS, PMON. POL.
 uint frequency_get_num_pstates();
-// Daemon required. Init required. | Not used without initialization. |
+// Daemon required. Init required. | Not used without initialization. | EARD, DVFS, PMON.
 ulong frequency_get_cpu_freq(uint cpu);
-// NEW
+// NEW | Daemon required. Init required. | Not used without initialization. | EARD.
 ulong frequency_get_cpufreq_list(uint cpus, ulong *cpuf);
-// Virtualizable. Init required. | Not used without initialization
+// Virtualizable. Init required. | Not used without initialization | EARL, PMON.
 ulong frequency_get_nominal_freq();
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | POL.
 ulong frequency_get_nominal_pstate();
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | EARD, PMON.
 ulong *frequency_get_freq_rank_list();
-// NEW
+// NEW | Daemon required. Init required. | Not used without initialization. |
 ulong frequency_set_cpu(ulong freq_khz, uint cpu);
-// Daemon required. Init required. | Not used without initialization.
+// Daemon required. Init required. | Not used without initialization. | EARD, DVFS, PMON.
 ulong frequency_set_all_cpus(ulong freq_khz);
-// Daemon required. Init required | Not used without initialization.
+// Daemon required. Init required | Not used without initialization. | EARD. | ear_set_cpufreq
 ulong frequency_set_with_mask(cpu_set_t *mask, ulong freq);
-// Daemon required. Init required. | Not used without initialization.
+// Daemon required. Init required. | Not used without initialization. | EARD. | eards_change_freq_with_list
 ulong frequency_set_with_list(uint cpus, ulong *cpuf);
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | EARD, EARL, PMON, DVFS. POL. |
 ulong frequency_pstate_to_freq(uint pstate_index);
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | EARL, DVFS.
 uint frequency_freq_to_pstate(ulong freq_khz);
 // Init not required.
 ulong frequency_pstate_to_freq_list(uint pstate_index, ulong *list, uint pstate_count);
 // Init not required.
 uint frequency_freq_to_pstate_list(ulong freq_khz, ulong *list, uint pstate_count);
-// Daemon required. Init required. | Not used without initialization.
+// Daemon required. Init required. | Not used without initialization. | EARD. | eards_set_turbo
 void frequency_set_performance_governor_all_cpus();
-// Daemon required. Init required. | Not used without initialization.
+// Daemon required. Init required. | Not used without initialization. | PMON.
 void frequency_set_userspace_governor_all_cpus();
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | EARD, PMON. POL.
 int frequency_is_valid_frequency(ulong freq_khz);
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | EARL.
 int frequency_is_valid_pstate(uint pstate);
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | EARL, PMON. POL.
 uint frequency_closest_pstate(ulong freq_khz);
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | POL.
 ulong frequency_closest_frequency(ulong freq_khz);
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | PMON, EARL.
 ulong frequency_closest_high_freq(ulong freq_khz, int pstate_minimum);
 
-// Virtualizable. Init required. | Not used without initialization.
+// Virtualizable. Init required. | Not used without initialization. | PMON.
 void get_governor(governor_t *_governor);
-// Daemon required. Init required. | Not used without initialization.
+// Daemon required. Init required. | Not used without initialization. | PMON.
 void set_governor(governor_t *_governor);
 
 #endif //MANAGEMENT_PSTATE_CONNECTOR
